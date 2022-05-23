899e22598772a7763301c675b83a656888a02bb4
Perryz2 <Perryz2@nycstudents.net>
2022-05-23 10:26:48 -0400

    commit

---
+++ project/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1 class= "title">Food Blog</h1>
+        <nav></nav>
+        <NuxtLink to="/">HomePage</NuxtLink>
+        <NuxtLink to="/about">BlogList</NuxtLink>
+    </header>
+</template>
+
+
+<style scoped>
+
+header {
+	padding: 20px 40px;
+	background-color: #FFF;
+	display: flex;
+	justify-content: space-between;
+	align-items: bottom;
+	box-shadow: 1px 4px 7px rgba(0, 0, 0, 0.1);
+	font-size: 28px;
+}
+.title {
+	color: #212121;
+	font-size: 30px;
+	font-weight: 900;
+	text-transform: uppercase;
+}
+
+</style>
+
